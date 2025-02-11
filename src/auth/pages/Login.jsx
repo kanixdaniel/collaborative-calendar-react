173a5c8bdb455107ec4809abb9ec63c62d5c616e
Daniel Perez Gonzalez <isc.pgd@gmail.com>
2025-02-10 20:36:00 -0600

    Feat: diseños de login y register v1

--- src/auth/pages/Login.jsx
+++ src/auth/pages/Login.jsx
@@ -1,5 +1,35 @@
+import { Link } from "react-router"
+
 export const Login = () => {
     return (
-        <div>Login</div>
+        <div className="container-fluid">
+            <div className="row align-items-center justify-content-center">
+                <div className="col-md-6">
+                    <h3 className="mb-4">Ingreso</h3>
+                    <form>
+                        <div className="form-group mb-3">
+                            <input
+                                type="text"
+                                className="form-control"
+                                placeholder="Correo"
+                            />
+                        </div>
+                        <div className="form-group mb-3">
+                            <input
+                                type="password"
+                                className="form-control"
+                                placeholder="Contraseña"
+                            />
+                        </div>
+                        <div className="form-group mb-2">
+                            <button className="btn btn-primary">
+                                Iniciar sesión
+                            </button>
+                        </div>
+                    </form>
+                    <p className="text-end">¿No tienes cuenta? <Link to="/auth/register">Crea una</Link></p>
+                </div>
+            </div>
+        </div>
     )
 }
--- src/auth/pages/Register.jsx
+++ src/auth/pages/Register.jsx
@@ -1,5 +1,50 @@
+import { Link } from "react-router"
+
+
 export const Register = () => {
     return (
-        <div>Register</div>
+        <div className="container-fluid">
+            <div className="row justify-content-center align-items-center">
+                <div className="col-md-6">
+                    <h3 className="text-center mb-4">Registro</h3>
+                    <form>
+                        <div className="form-group mb-3">
+                            <input
+                                type="text"
+                                className="form-control"
+                                placeholder="Nombre"
+                            />
+                        </div>
+                        <div className="form-group mb-3">
+                            <input
+                                type="email"
+                                className="form-control"
+                                placeholder="Correo"
+                            />
+                        </div>
+                        <div className="form-group mb-3">
+                            <input
+                                type="password"
+                                className="form-control"
+                                placeholder="Contraseña"
+                            />
+                        </div>
+
+                        <div className="form-group mb-3">
+                            <input
+                                type="password"
+                                className="form-control"
+                                placeholder="Repita la contraseña"
+                            />
+                        </div>
+
+                        <button className="btn btn-primary">
+                            Crear cuenta
+                        </button>
+                    </form>
+                    <p className="text-end">¿Ya tienes una cuenta? <Link to="/auth/login">Inicia sesión</Link></p>
+                </div>
+            </div>
+        </div>
     )
 }
